679f52b9fa61b5a1fe4912f31608fbad03829239
Sayan Biswas <sayan@pokurt.me>
2021-11-27 03:35:14 +0530

    utils: new method to parse text between 2 strings
    Signed-off-by: Sayan Biswas <sayan@pokurt.me>

--- utils/utils.go
+++ utils/utils.go
@@ -9,6 +9,7 @@ import (
 	"math/rand"
 	"net/http"
 	"os"
+	"strings"
 	"time"
 )
 
@@ -238,4 +239,18 @@ func GetAddCommand() string {
 
 func GetRemoveCommand() string {
 	return CommandConfig.REMOVE
+}
+
+func GetStringInBetweenTwoString(str string, startS string, endS string) (result string, found bool) {
+	s := strings.Index(str, startS)
+	if s == -1 {
+		return result, false
+	}
+	newS := str[s+len(startS):]
+	e := strings.Index(newS, endS)
+	if e == -1 {
+		return result, false
+	}
+	result = newS[:e]
+	return result, true
 }

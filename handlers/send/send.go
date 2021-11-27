464e7efd7c5a99f20db5b9e68ea26db8e7eda380
Sawada Tsunayoshi <34431649+TsunayoshiSawada@users.noreply.github.com>
2021-11-27 13:59:54 +0530

    Back to #### and $$$$ for image parsing ixes

--- handlers/send/send.go
+++ handlers/send/send.go
@@ -143,7 +143,7 @@ func SendHandler(b ext.Bot, u *gotgbot.Update) error {
 
 	torep := ""
 
-	if p, f := utils.GetStringInBetweenTwoString(message, "{image:", ":imageend}"); f {
+	if p, f := utils.GetStringInBetweenTwoString(message, "####", "$$$$}"); f {
 		rq := b.NewSendablePhoto(utils.GetChannelId(), "")
 		rq.Photo = b.NewFileURL(p)
 		rq.ParseMode = "HTML"
